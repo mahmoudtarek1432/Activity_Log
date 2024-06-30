ddcc59de7714d351e8bb8d1e1095c68c74c45b19
Mahmoud Tarek <mtarek@spark-sys.com>
2024-06-30 18:57:29 +0300

    implemented table and skeleton styling

--- activity_logger/tailwind.config.ts
+++ activity_logger/tailwind.config.ts
@@ -13,8 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "DarkGray" : "#1c1c1c",
+        "hoverGray" : "#FBFBFB"
+      }
     },
+
   },
-  plugins: [],
+  plugins: [require('tailwindcss-font-inter')]
 };
 export default config;
